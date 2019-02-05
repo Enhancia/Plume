ff5352d297c9a3042e0a0766a00b493106b3c80e
alexlevacher <a.levacher@hotmail.fr>
2019-02-05 10:25:07 +0100

    Ajout case mac pour envoi mail

--- Plume/Source/Ui/Top/OptionsPanel.cpp
+++ Plume/Source/Ui/Top/OptionsPanel.cpp
@@ -116,9 +116,8 @@ void OptionsPanel::resized()
     scanArea.removeFromLeft (scanArea.getWidth()/3); // "Scan Plugins" text
     
     scanner->setBounds (scanArea);
-  #if JUCE_WINDOWS
+    
     mailButton->setBounds (area.removeFromBottom (30 + 2*MARGIN).reduced (4*MARGIN, MARGIN).withWidth (60));
-  #endif
 }
 
 //==============================================================================
@@ -140,7 +139,6 @@ void OptionsPanel::buttonClicked (Button* bttn)
     
     if (bttn == mailButton)
     {
-      #if JUCE_WINDOWS
         if (auto* plumeLogger = dynamic_cast<FileLogger*> (Logger::getCurrentLogger()))
         {
             String fullLog = plumeLogger->getLogFile().loadFileAsString().removeCharacters ("\n");
@@ -148,22 +146,27 @@ void OptionsPanel::buttonClicked (Button* bttn)
             /* Only keeps the last 3 entries of the log: the one that had an issue, 
                the one used to send the report, and one more to cover cases where the plugin
                has to be checked (For instance the plugin check when launching Ableton..)
-               If too long, */
+               If too long, keeps the 6000 last characters.. */
             int startIndex = jmax (fullLog.upToLastOccurrenceOf("Plume Log", false, false)
-		                                  .upToLastOccurrenceOf("Plume Log", false, false)
+                                          .upToLastOccurrenceOf("Plume Log", false, false)
 								          .lastIndexOf("Plume Log"),
-								   fullLog.length() - 6000);
-								  
+								          fullLog.length() - 6000);
+								          
 			String mail_str ("mailto:damien.leboulaire@enhancia.co"
                              "?Subject=[Plume Report]"
 		                     "&body=" + fullLog.substring (startIndex));
-		                               
+		  #if JUCE_WINDOWS
 		    LPCSTR mail_lpc = mail_str.toUTF8();
 
             ShellExecute (NULL, "open", mail_lpc,
 		                  "", "", SW_SHOWNORMAL);
+		  #elif JUCE_MAC
+		    mail_str = "open " + mail_str;
+		    CharPointer_UTF8 mail_ptr = mail_str.toUTF8();
+		    
+		    system (mail_ptr);
+		  #endif
         }
-      #endif
     }
 }
 
