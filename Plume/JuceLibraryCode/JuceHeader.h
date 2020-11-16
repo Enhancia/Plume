e698cb4fbab66c6949d019f1838f5858f4113640
Alex Levacher <a.levacher@hotmail.fr>
2020-11-16 10:49:46 +0100

    Added juce 6 license and modules

--- Plume/JuceLibraryCode/JuceHeader.h
+++ Plume/JuceLibraryCode/JuceHeader.h
@@ -52,7 +52,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Plume";
     const char* const  companyName    = "Enhancia";
-    const char* const  versionString  = "1.4.1";
-    const int          versionNumber  = 0x10401;
+    const char* const  versionString  = "1.5.0";
+    const int          versionNumber  = 0x10500;
 }
 #endif
