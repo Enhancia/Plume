223714af3bd7bae82f0f33aae8a1934264650e86
Alex Levacher <a.levacher@hotmail.fr>
2021-03-10 16:43:24 +0100

    Now uses plume file's folder as a system folder for scanning plugins.

--- Plume/Source/Wrapper/PluginWrapper.cpp
+++ Plume/Source/Wrapper/PluginWrapper.cpp
@@ -376,6 +376,11 @@ Array<File*> PluginWrapper::createFileList()
     {
         if (fileToAdd.exists()) dirs.add (new File (fileToAdd));
     };
+
+    if (useDefaultPaths)
+    {
+        addFileToDirectories (directories, File::getSpecialLocation (File::currentExecutableFile).getParentDirectory());
+    }
     
     // Adds OS specific paths if they exist
   #if JUCE_WINDOWS
