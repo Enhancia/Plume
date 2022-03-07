43393a216a8e1291e9282bc5b8cb0a71741c0e43
Alex Levacher <a.levacher@hotmail.fr>
2022-03-07 16:18:48 +0100

    Changed juceheader

--- Plume/JuceLibraryCode/JuceHeader.h
+++ Plume/JuceLibraryCode/JuceHeader.h
@@ -52,7 +52,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Plume";
     const char* const  companyName    = "Enhancia";
-    const char* const  versionString  = "100.1.15";
-    const int          versionNumber  = 0x64010f;
+    const char* const  versionString  = "100.2.0";
+    const int          versionNumber  = 0x640200;
 }
 #endif
