57bdffae4798504f2e16901baacb56331b5f56a5
Alex Levacher <a.levacher@hotmail.fr>
2022-03-28 14:55:07 +0200

    Corrected issue causing vibrato to not be displayed properly

--- Plume/Source/Ui/Gesture/Tuner/VibratoTuner.cpp
+++ Plume/Source/Ui/Gesture/Tuner/VibratoTuner.cpp
@@ -367,7 +367,7 @@ void VibratoTuner::drawValueCursor (Graphics& g)
 	lastValue = value;
 
 	int offset = (getIntensity() < getThreshold()) ? 0
-	                                          : static_cast<int>(value - 0.5f) * (gainSlider->getWidth() - 30)
+	                                          : static_cast<int> ((value - 0.5f) * (static_cast<float> (gainSlider->getWidth()) - 30.0f))
 	                                                           * static_cast<int>(getGain())/85;
 
 	juce::Point<int> cursorPoint = {gainSlider->getBounds().getCentreX() + offset,
